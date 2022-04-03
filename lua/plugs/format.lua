163c5ca44805c3108bda887462d366e51c8dd493
Falcon <leonsout@outlook.com>
2022-04-03 18:26:03 +0800

    添加json文件自动格式化

--- lua/plugs/format.lua
+++ lua/plugs/format.lua
@@ -38,6 +38,15 @@ require("formatter").setup(
           }
         end
       },
+      json = {
+        function()
+          return {
+            exe = "prettier",
+            args = {"--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)), "--single-quote"},
+            stdin = true
+          }
+        end
+      },
       html = {
         function()
           return {
